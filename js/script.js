cc4f8c9bcbd83f8e3e1383d23abab1a11bbb74f6
Vladimir Ascencio <vladimirramos405@gmail.com>
2025-01-16 16:19:16 -0600

    Cuarto avance

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+
+const activePage = () => {
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+}
+
+navLinks.forEach((link, idx) => {
+    link.addEventListener('click', () => {
+        if(!link.classList.contains('active')){
+            activePage();
+            link.classList.add('active');
+        }
+    });
+});
+
+
+logoLink.addEventListener('click', () =>{
+    if(!navLinks[0].classList.contains('active')){
+        activePage();
+        navLinks[0].classList.add('active');
+    }
+});
+
+
+
 // Codigo para que el color verde del boton pase a otro al dar click
 const resumeBtns = document.querySelectorAll('.resume-btn');
 
@@ -15,4 +43,48 @@ resumeBtns.forEach((btn,idx) =>{
         });
         resumeDetails[idx].classList.add('active');
     });
+});
+
+// Código para que el carrusel de portfolios se mueva al dar clic en los botones
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0; // Índice inicial
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+    
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active'); // Mostrar la información del portfolio correspondiente al índice actual 
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < 4) {
+        index++;
+        arrowLeft.classList.remove('disabled'); // Habilitar la flecha izquierda si avanzamos
+    }
+    else{
+        index = 5;
+        arrowRight.classList.add('disabled'); // Deshabilitar la flecha derecha si llegamos al final
+    }
+    
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+        arrowRight.classList.remove('disabled'); // Habilitar la flecha derecha si retrocedemos
+    }
+    else{
+        index = 0;
+        arrowLeft.classList.add('disabled'); // Deshabilitar la flecha izquierda si llegamos al principio
+    }
+
+    activePortfolio();
 });
