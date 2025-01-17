1cc7e55f99866a7004f221a817edd7933cbcc47f
Vladimir Ascencio <vladimirramos405@gmail.com>
2025-01-17 14:20:56 -0600

    Último progreso, página funcional

--- js/script.js
+++ js/script.js
@@ -1,10 +1,40 @@
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
 
 const activePage = () => {
+    const header = document.querySelector('header');
+    const barsBox = document.querySelector('.bars-box');
+
+    header.classList.remove('active');
+    setTimeout(() =>{
+        header.classList.add('active');
+    },1100);
+
+
     navLinks.forEach(link => {
         link.classList.remove('active');
     });
+
+    barsBox.classList.remove('active');
+    setTimeout(() => {
+        barsBox.classList.add('active');
+    },1100);
+
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 navLinks.forEach((link, idx) => {
@@ -12,6 +42,9 @@ navLinks.forEach((link, idx) => {
         if(!link.classList.contains('active')){
             activePage();
             link.classList.add('active');
+            setTimeout(() =>{
+                sections[idx].classList.add('active');
+            },1100);
         }
     });
 });
@@ -21,10 +54,11 @@ logoLink.addEventListener('click', () =>{
     if(!navLinks[0].classList.contains('active')){
         activePage();
         navLinks[0].classList.add('active');
+        setTimeout(() =>{
+            sections[0].classList.add('active');
+        },1100);
     }
 });
-
-
 
 // Codigo para que el color verde del boton pase a otro al dar click
 const resumeBtns = document.querySelectorAll('.resume-btn');
@@ -72,7 +106,6 @@ arrowRight.addEventListener('click', () => {
         index = 5;
         arrowRight.classList.add('disabled'); // Deshabilitar la flecha derecha si llegamos al final
     }
-    
     activePortfolio();
 });
 
@@ -85,6 +118,5 @@ arrowLeft.addEventListener('click', () => {
         index = 0;
         arrowLeft.classList.add('disabled'); // Deshabilitar la flecha izquierda si llegamos al principio
     }
-
     activePortfolio();
 });
